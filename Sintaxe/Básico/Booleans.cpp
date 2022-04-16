ba8340c2347075bf8560382e59c3d03246a2502f
israel souza <souzaferreira437@gmail.com>
2022-04-16 17:51:10 -0300

    comitado

--- Sintaxe/Básico/Booleans.cpp
+++ Sintaxe/Básico/Booleans.cpp
@@ -2,6 +2,6 @@
 int main(){
     int numero = 5;
     bool teste;
-    numero == 5 ? teste = true: teste = false;    // operador ternário
+    numero == 5 ? teste = true: teste = false;    //  operador ternário
     std::cout << teste;
 }
